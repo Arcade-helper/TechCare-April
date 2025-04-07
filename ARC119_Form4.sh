f06ff3964e9c0060444c8223eb895dd4ea3cdf7e
Arcade-helper <arcadehelper1418@gmail.com>
2025-04-08 04:15:39 +0530

    Create ARC119_Form4.sh

---
+++ ARC119_Form4.sh
@@ -0,0 +1,28 @@
+export REGION="${ZONE%-*}"
+
+gcloud alpha dataplex lakes create customer-lake \
+--display-name="Customer-Lake" \
+ --location=$REGION \
+ --labels="key_1=$KEY_1,value_1=$VALUE_1"
+
+gcloud dataplex zones create public-zone \
+    --lake=customer-lake \
+    --location=$REGION \
+    --type=RAW \
+    --resource-location-type=SINGLE_REGION \
+    --display-name="Public-Zone"
+
+gcloud dataplex assets create customer-raw-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=STORAGE_BUCKET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/buckets/$DEVSHELL_PROJECT_ID-customer-bucket \
+            --discovery-enabled \
+            --display-name="Customer Raw Data"
+
+gcloud dataplex assets create customer-reference-data --location=$REGION \
+            --lake=customer-lake --zone=public-zone \
+            --resource-type=BIGQUERY_DATASET \
+            --resource-name=projects/$DEVSHELL_PROJECT_ID/datasets/customer_reference_data \
+            --display-name="Customer Reference Data"
+
+echo "${CYAN}${BOLD}Click here: "${RESET}""${BLUE}${BOLD}"https://console.cloud.google.com/dataplex/lakes/customer-lake/zones/public-zone/create-entity;location=$REGION?project=$DEVSHELL_PROJECT_ID""${RESET}"
